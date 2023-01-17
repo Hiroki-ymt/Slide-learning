2b52629a4b62d0fc7399f3954d0766dda89668c0
Hiroki Ymt <94109647+Hiroki-ymt@users.noreply.github.com>
2023-01-17 20:34:38 +0900

    なんかいろいろ試行錯誤

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Reveal from "libs/reveal.js/dist/reveal.js";
-import Markdown from "libs/reveal.js/plugin/markdown/markdown.js";
-import Notes from "libs/reveal.js/plugin/notes/notes.js";
-import Highlight from "libs/reveal.js/plugin/highlight/highlight.js";
+import Reveal from      "../node_modules/reveal.js/dist/reveal.js";
+import Markdown from    "../node_modules/reveal.js/plugin/markdown/markdown.esm.js";
+import Notes from       "../node_modules/reveal.js/plugin/notes/notes.esm.js";
+import Highlight from   "../node_modules/reveal.js/plugin/highlight/highlight.esm.js";
+
+import "./main.css";
 
 const deck = new Reveal({
-    plugins: [Markdown, Highlight, Notes],
+    plugins: [ Markdown, Highlight, Notes ],
     history: true
 });
 deck.initialize();
