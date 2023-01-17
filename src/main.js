fe0236c687540bc87d33861bb5bfa3cbdb696ea1
Hiroki Ymt <94109647+Hiroki-ymt@users.noreply.github.com>
2023-01-17 21:24:49 +0900

    moduleを呼び出せないという悩み

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Reveal from      "../node_modules/reveal.js/dist/reveal.js";
+import Reveal from      "../node_modules/reveal.js/dist/reveal.esm.js";
 import Markdown from    "../node_modules/reveal.js/plugin/markdown/markdown.esm.js";
 import Notes from       "../node_modules/reveal.js/plugin/notes/notes.esm.js";
 import Highlight from   "../node_modules/reveal.js/plugin/highlight/highlight.esm.js";
